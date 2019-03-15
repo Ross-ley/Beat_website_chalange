7cb5ed65f01e93a0627f952b9c0ab669a794aad1
Ross <rbunn1997@gmail.com>
2019-03-15 09:51:34 +0000

    I have updated and added more style to the page

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,21 @@
 
+// below is the code for the side menu
+/* side menu animation*/ 
+function myFunction(x) { 
+  x.classList.toggle("change");
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
 
 // below is the code used for the cookie.
 
